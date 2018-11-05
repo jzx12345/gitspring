839e460b8ad1243bba23c0062a1fcdd0357f482b
zhansan <123456>
2018-11-05 21:29:14 +0800

    第一次提交

--- firstclass.java
+++ firstclass.java
@@ -4,4 +4,7 @@ public class firstclass {
     public static void main(String[] args) {
         System.out.println("haohaohaoxuexi,haohaogongzuo");
     }
+    public  void test(){
+        System.out.println("haohaogongzuo");
+    }
 }
